9eb6ad52567cb8c45c23b99314b13cedad885067
Ariel Buchhamer <arielbuchhamer1@outlook.com>
2025-07-29 21:06:59 -0300

    Agrega Cors para la visibilidad del front

--- src/main/java/com/BackTecnophones/SecurityConfig.java
+++ src/main/java/com/BackTecnophones/SecurityConfig.java
@@ -1,15 +1,25 @@
 package com.BackTecnophones;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 public class SecurityConfig {
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+	        .cors(cors -> cors.configurationSource(request -> {
+	            CorsConfiguration config = new CorsConfiguration();
+	            config.setAllowedOrigins(List.of("http://localhost:5173"));
+	            config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+	            config.setAllowedHeaders(List.of("*"));
+	            return config;
+	        }))
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .anyRequest().permitAll()
