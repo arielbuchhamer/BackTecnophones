b7764034e9d8e09ac0844400b0c1464af13c6ba3
Arielb11 <arielbuchhamer1@outlook.com>
2025-10-21 21:48:16 -0300

    Mejora los metodos al traer articulos y categorias

--- src/main/java/com/BackTecnophones/repository/CategoriaRepository.java
+++ src/main/java/com/BackTecnophones/repository/CategoriaRepository.java
@@ -2,6 +2,7 @@ package com.BackTecnophones.repository;
 
 import java.util.List;
 
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,20 @@ import com.BackTecnophones.model.Categoria;
 @Repository
 public interface CategoriaRepository  extends MongoRepository<Categoria, String>{
 	List<Categoria> findByRubroId(String rubroId);
+	
 	List<Categoria> findAllByOrderByIdDesc();
+
+	@Aggregation(pipeline = {
+			  // convertir rubroId (String) → ObjectId
+			  "{ $addFields: { rubroIdObj: { $convert: { input: \"$rubroId\", to: \"objectId\", onError: null, onNull: null } } } }",
+
+			  // ahora sí matchea con _id (ObjectId) de 'rubros'
+			  "{ $lookup: { from: \"rubros\", localField: \"rubroIdObj\", foreignField: \"_id\", as: \"rubro\" } }",
+			  "{ $unwind: { path: \"$rubro\", preserveNullAndEmptyArrays: true } }",
+
+			  "{ $project: { _id: 1, descripcion: 1, rubroId: 1, " +
+			    "rubroDescripcion: { $ifNull: [ \"$rubro.descripcion\", \"\" ] } } }"
+			})
+			List<Categoria> findAllEntityWithRubroDescripcion();
+
 }
